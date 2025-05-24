4d6eb10fbb6fc3603196135f43676288e01ed861
Jens Farronato <jens@MacBookPro.fritz.box>
2025-05-24 21:57:37 +0200

    Weitere Fragen ergänzt

--- api.js
+++ api.js
@@ -1,30 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-app.use(cors());
-
 const fragen = [
-  {
-    id: 1,
-    frage: "Wie heisst die Hauptstadt der Schweiz?",
-    antworten: {
-      A: "Basel",
-      B: "Bern",
-      C: "Zürich",
-      D: "Luzern"
+    {
+      id: 1,
+      frage: "Wie heisst die Hauptstadt der Schweiz?",
+      antworten: {
+        A: "Basel",
+        B: "Bern",
+        C: "Zürich",
+        D: "Luzern"
+      },
+      korrekt: "B"
     },
-    korrekt: "B"
-  }
-];
-
-app.get('/frage/:id', (req, res) => {
-  const frage = fragen.find(f => f.id === parseInt(req.params.id));
-  if (frage) {
-    res.json(frage);
-  } else {
-    res.status(404).json({ error: "Frage nicht gefunden" });
-  }
-});
-
-module.exports = app;
+    {
+      id: 2,
+      frage: "Welches Tier kann fliegen?",
+      antworten: {
+        A: "Hund",
+        B: "Pinguin",
+        C: "Fledermaus",
+        D: "Kuh"
+      },
+      korrekt: "C"
+    },
+    {
+      id: 3,
+      frage: "Wie viele Tage hat eine Woche?",
+      antworten: {
+        A: "5",
+        B: "6",
+        C: "7",
+        D: "8"
+      },
+      korrekt: "C"
+    }
+  ]
+  
